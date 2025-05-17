4c4932af8265d97a760db69d56e3abc5414c03bd
Armaandeep <105118201+armaan-dot@users.noreply.github.com>
2025-05-17 09:47:32 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+'''
+
+                            Online Python Compiler.
+                Code, Compile, Run and Debug python program online.
+Write your code in this editor and press "Run" button to execute it.
+
+'''
+
+a = 0
+b = 1
+s = 0
+for n in range(12):
+    print(a)
+    s=a  
+    a=b  
+    b=s+a 
+    
