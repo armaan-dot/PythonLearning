74318078235106b380b0f420b109ecb48ee6ae59
Armaandeep <105118201+armaan-dot@users.noreply.github.com>
2025-05-17 09:48:38 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,3 @@
-'''
-
-                            Online Python Compiler.
-                Code, Compile, Run and Debug python program online.
-Write your code in this editor and press "Run" button to execute it.
-
-'''
-
 a = 0
 b = 1
 s = 0
